cc5de65887b9a3295b39a4706b0d90c9d01cf083
Sigimatika <sigitechindo@gmail.com>
2024-05-24 20:22:53 +0700

    regre

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -42,35 +42,35 @@ class DatabaseSeeder extends Seeder
                 'is_admin' => 'super_admin',
             ]);
 
-            Category::factory()->create([
-                'nama' => 'Makanan',
-                'slug' => '-makanan',
-                // 'icon' => 'icon-food.png', // Example icon, replace with actual file path if needed
-                'created_at' => now(),
-                'updated_at' => now(),
-            ]);
-            Category::factory()->create([
-                'nama' => 'Kesehatan',
-                'slug' => '-kesehatan',
-                // 'icon' => 'icon-food.png', // Example icon, replace with actual file path if needed
-                'created_at' => now(),
-                'updated_at' => now(),
-            ]);
-            Category::factory()->create([
-                'nama' => 'Pendidikan',
-                'slug' => '-pendidikan',
-                // 'icon' => 'icon-food.png', // Example icon, replace with actual file path if needed
-                'created_at' => now(),
-                'updated_at' => now(),
-            ]);
+            // Category::factory()->create([
+            //     'nama' => 'Makanan',
+            //     'slug' => '-makanan',
+            //     // 'icon' => 'icon-food.png', // Example icon, replace with actual file path if needed
+            //     'created_at' => now(),
+            //     'updated_at' => now(),
+            // ]);
+            // Category::factory()->create([
+            //     'nama' => 'Kesehatan',
+            //     'slug' => '-kesehatan',
+            //     // 'icon' => 'icon-food.png', // Example icon, replace with actual file path if needed
+            //     'created_at' => now(),
+            //     'updated_at' => now(),
+            // ]);
+            // Category::factory()->create([
+            //     'nama' => 'Pendidikan',
+            //     'slug' => '-pendidikan',
+            //     // 'icon' => 'icon-food.png', // Example icon, replace with actual file path if needed
+            //     'created_at' => now(),
+            //     'updated_at' => now(),
+            // ]);
 
-            Category::factory()->create([
-                'nama' => 'Infrastruktur',
-                'slug' => '-infrastruktur',
-                // 'icon' => 'icon-food.png', // Example icon, replace with actual file path if needed
-                'created_at' => now(),
-                'updated_at' => now(),
-            ]);
+            // Category::factory()->create([
+            //     'nama' => 'Infrastruktur',
+            //     'slug' => '-infrastruktur',
+            //     // 'icon' => 'icon-food.png', // Example icon, replace with actual file path if needed
+            //     'created_at' => now(),
+            //     'updated_at' => now(),
+            // ]);
 
             Lokasimakangratis::factory()->create([
                 'kota' => 'Cileunyi',
